b0f72e868d24a69af18140c06cae63598944e3d0
yaseen454 <117825343+yaseen454@users.noreply.github.com>
2024-09-09 19:05:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -43,8 +43,7 @@ def clipboard_code():
     # Display images and extracted text only after done pasting
     if st.session_state.done_pasting:
         if st.session_state['texts']:
-            for text in st.session_state['texts']:
-                st.write(f"Extracted Text: {text}")
+            st.write(f"Extracted Text: {text}")
 
         # Buttons for managing images/texts and resetting the state
         col1, col2, col3 = st.columns(3)
